4fb1d65916b9d2b97fe01cd758e786d17efc479a
Zheng Hua <writeforever@foxmail.com>
2022-01-20 21:29:48 +0800

    update utils.
    Signed-off-by: Zheng Hua <writeforever@foxmail.com>

--- source/include/utils.h
+++ source/include/utils.h
@@ -1,17 +1,17 @@
 // Copyright (C) 2021 jensenhua(writeforever@foxmail.com)
-// 
+//
 // This file is part of sdol.
-// 
+//
 // sdol is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 2 of the License, or
 // (at your option) any later version.
-// 
+//
 // sdol is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with sdol.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -22,7 +22,7 @@
 *      INCLUDES
 *********************/
 #ifndef STCMCU_H
-#include "stcmcu.h"
+    #include "stcmcu.h"
 #endif
 
 /*********************
@@ -40,9 +40,9 @@
 /**********************
 * GLOBAL PROTOTYPES
 **********************/
-int32_t simple_atoi(char s[]);
+int32_t simple_atoi( char s[] );
 
-uint8_t simple_lower(uint8_t c);
-uint8_t simple_isdigit(uint8_t c);
+uint8_t simple_lower( uint8_t c );
+uint8_t simple_isdigit( uint8_t c );
 
 #endif
