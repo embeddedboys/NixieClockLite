d5837eb6053d6557c40adc283fcb9ded11aafd78
Zheng Hua <writeforever@foxmail.com>
2022-01-20 21:26:23 +0800

    add hal.h
    Signed-off-by: Zheng Hua <writeforever@foxmail.com>

---
+++ source/include/hal.h
@@ -0,0 +1,24 @@
+// Copyright (C) 2022 16578
+//
+// SDOL is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// SDOL is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with SDOL. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef __HAL_H
+#define __HAL_H
+
+typedef enum {
+    HAL_LOCKED   = 0,
+    HAL_UNLOCKED = 1,
+} hal_lock_t;
+
+#endif
