c2888466908425e74ac430bbd7fb2aa31e3c6b5f
Zheng Hua <writeforever@foxmail.com>
2022-01-25 02:59:14 +0800

    update test program.
    Signed-off-by: Zheng Hua <writeforever@foxmail.com>

--- source/tests/test_ds1302_capability.c
+++ source/tests/test_ds1302_capability.c
@@ -1,24 +1,24 @@
 /**
  * @file test_ds1302_capability.c
  * @author Zheng Hua (writeforever@foxmail.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-01-22
- * 
+ *
  * MIT License
- * 
+ *
  * Copyright 2022 Zheng Hua(writeforever@foxmail.com)
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,7 +26,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- * 
+ *
  */
 
 #include "ds1302.h"
@@ -77,10 +77,38 @@ void Timer2_Isr() interrupt 12
     }
 }
 
+void Delay1000ms()      //@24.000MHz
+{
+    unsigned char i, j, k;
+    
+    _nop_();
+    _nop_();
+    i = 122;
+    j = 193;
+    k = 128;
+    
+    do
+    {
+        do
+        {
+            while( --k );
+        }
+        while( --j );
+    }
+    while( --i );
+}
+
 void main( void )
 {
     uint8_t loop = 1;
-
+    
     SystemInit();
     
+    while( 1 ) {
+        __ds1302_set_rst(1);
+        __ds1302_writebyte(0x82);
+        __ds1302_writebyte(0x11);
+        __ds1302_set_rst(0);
+        Delay1000ms();
+    }
 }
